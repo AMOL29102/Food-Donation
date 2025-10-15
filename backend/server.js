a3417984b59dd204cedcbfc7d1197b7b11230da7
Amol <amolpatilamp2910@gmail.com>
2025-10-15 10:14:21 +0530

    Hosting

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,5 @@ app.use('/api/provider', providerRoutes);
 app.use('/api/consumer', consumerRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, console.log(`Server running on port ${PORT}`));
+export default app;
